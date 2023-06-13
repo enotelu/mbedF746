a925738cdc98a244c6276ad65e5db6b81df7a8fb
enotelu <thibault.laval3@gmail.com>
2023-06-13 14:57:53 +0200

    deleted:    lib/WS2812/WS2812.cpp 	deleted:    lib/WS2812/WS2812.h 	deleted:    lib/WS2812/cRGB.h 	deleted:    lib/WS2812/examples/Blinky/Blinky.ino 	deleted:    lib/WS2812/examples/RGB_Clock/RGB_Clock.ino 	deleted:    lib/WS2812/examples/fade_rgb/fade_rgb.ino 	deleted:    lib/WS2812/examples/rainbow/rainbow.ino 	deleted:    lib/WS2812/library.json 	deleted:    lib/WS2812/light_ws2812.cpp 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 #include "demos/lv_demos.h"
 #include <cstdio>
 #include <lvgl.h>
+#include <examples/lv_examples.h>
+
+
 
 
 ThreadLvgl threadLvgl(30);
+lv_obj_t * bar;
 
 void texte (void) {
 
@@ -21,24 +25,65 @@ void colorwheel(void) {
     lv_obj_t * cw;
 
     cw = lv_colorwheel_create(lv_scr_act(), true);
-    lv_obj_set_size(cw, 200, 200);
-    lv_obj_center(cw);
+    lv_obj_set_size(cw, 245, 245);
+    lv_obj_align(cw, LV_ALIGN_CENTER, 0,0);
 }
+
+static void set_temp(lv_obj_t * bar, int32_t temp)
+{
+    lv_bar_set_value(bar, temp, LV_ANIM_ON);
+}
+
+/**
+ * A temperature meter example
+ */
+void bar_3(void)
+{
+    static lv_style_t style_indic;
+
+    lv_style_init(&style_indic);
+    lv_style_set_bg_opa(&style_indic, LV_OPA_COVER);
+    lv_style_set_bg_color(&style_indic, lv_palette_main(LV_PALETTE_RED));
+    lv_style_set_bg_grad_color(&style_indic, lv_palette_main(LV_PALETTE_GREEN));
+    lv_style_set_bg_grad_dir(&style_indic, LV_GRAD_DIR_VER);
+
+    bar = lv_bar_create(lv_scr_act());
+    lv_obj_add_style(bar, &style_indic, LV_PART_INDICATOR);
+    lv_obj_set_size(bar, 20, 200);
+    lv_obj_align(bar, LV_ALIGN_OUT_LEFT_MID, 2,0);
+    lv_bar_set_range(bar, -10, 100);
+
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_exec_cb(&a, 0);
+    lv_anim_set_time(&a, 3000);
+    lv_anim_set_playback_time(&a, 3000);
+    lv_anim_set_var(&a, bar);
+    lv_anim_set_values(&a, -10, 100);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_start(&a);
+}
+
 
 
 int main() {
 
     AnalogIn micro(A0);
     DigitalIn gate(D2);
-    
+
+    float microPourcentage = 0.0;
 
     texte();
     colorwheel();
+    bar_3();
+
+    
 
 
     while(1) {
         float microValue = micro.read();
-        float microPourcentage = microValue*100;
+        microPourcentage = microValue*110.0;
+        set_temp(bar,microPourcentage);
         
         if (microValue>0.7 && gate==1){
             printf("Le son est FORT\n");
@@ -53,10 +98,11 @@ int main() {
             printf("Le son est FAIBLE\n");
             printf("%.2f\n", static_cast<double>(microValue));
         }
+        lv_bar_set_value(bar, microPourcentage, LV_ANIM_OFF);
 
         
 
-        ThisThread::sleep_for(2ms);
+        ThisThread::sleep_for(1ms);
 
     }
 }
